dae152a594698160c869341ecd9a717adf27d0e0
Nicholas Pleis <npleis@riotgames.com>
2018-07-27 13:55:42 -0700

    Updated workerbee to use new format!

--- workerbee/workerbee.ino
+++ workerbee/workerbee.ino
@@ -23,6 +23,33 @@ uint8_t lightMode = 0;
 char dataBuffer[DATA_BUFFER_LENGTH];
 uint16_t dataOffset = 0;
 
+// The app sends a string containing all of the information we need to execute a pattern.
+// The string is formatted as PATTERN,SPEED,BRIGHTNESS,R,G,B
+// PATTERN: 4 character code indicating the pattern to display
+// SPEED: Number from 1-100 giving the speed
+// BRIGHTNESS: Number from 1-100 giving the brightness
+// R,G,B: Numbers giving the single byte value for Red, Green, and Blue 
+
+struct CmdParams
+{
+  CmdParams()
+  {
+    mPattern = FX_MODE_THEATER_CHASE_RAINBOW;
+    mSpeed = 50;
+    mBrightness = 50;
+    mRed = 255;
+    mGreen = 0;
+    mBlue = 0;
+  }
+  
+  String mPattern;
+  int mSpeed;
+  int mBrightness;
+  unsigned char mRed;
+  unsigned char mBlue;
+  unsigned char mGreen;
+};
+
 void setup() {
   Serial.begin(115200);
   Serial.print("Buzzkill Worker Bee: ");
@@ -134,23 +161,118 @@ void reset() {
   dataOffset = 0;
 }
 
-void runCommand() {
-  const char* pattern = (const char*)(dataBuffer);
-  Serial.print("PATTERN:");
-  Serial.println(pattern);
+void parseParams(CmdParams& params, char* cmdString)
+{
+  Serial.println("STARTING PARSING");
+  char* rawPattern = strtok(cmdString, ",");
+  if (rawPattern != NULL)
+  {
+    params.mPattern = rawPattern;
+    params.mPattern.toUpperCase();
+  }
 
-  uint8_t mode = modeForPattern(pattern);
+  char* rawSpeed = strtok(NULL, ",");
+  if (rawSpeed != NULL)
+  {
+    String strSpeed = rawSpeed;
+    params.mSpeed = (int)strSpeed.toInt();
+    if (params.mSpeed < 1)
+    {
+      params.mSpeed = 1;
+    }
+    else if(params.mSpeed >= 100)
+    {
+      params.mSpeed = 100;
+    }
+  }
+
+  char* rawBright = strtok(NULL, ",");
+  if (rawBright != NULL)
+  {
+    String strBright = rawBright;
+    params.mBrightness = (int)strBright.toInt();
+
+    if (params.mBrightness < 0)
+    {
+      params.mBrightness = 0;
+    }
+    else if(params.mBrightness >= 100)
+    {
+      params.mBrightness = 100;
+    }
+  }
+
+  char* rawRed = strtok(NULL, ",");
+  if (rawRed != NULL)
+  {
+    String strRed = rawRed;
+    params.mRed = (char)strRed.toInt();
+  }
+
+  char* rawGreen = strtok(NULL, ",");
+  if (rawGreen != NULL)
+  {
+    String strGreen = rawGreen;
+    params.mGreen = (char)strGreen.toInt();
+  }
+
+  char* rawBlue = strtok(NULL, ",");
+  if (rawBlue != NULL)
+  {
+    String strBlue = rawBlue;
+    params.mBlue = (char)strBlue.toInt();
+  }
+}
+
+void runCommand()
+{
+  CmdParams params;
+  parseParams(params, (char*)dataBuffer);
+
+  Serial.print("PATTERN:");
+  Serial.println(params.mPattern);
+
+  uint8_t mode = modeForPattern(params.mPattern);
   Serial.print("MODE: ");
   Serial.println(mode);
 
+  Serial.print("SPEED: ");
+  Serial.println(params.mSpeed);
+
+  Serial.print("BRIGHTNESS: ");
+  Serial.println(params.mBrightness);
+
+  Serial.print("RED: ");
+  Serial.println(params.mRed);
+
+  Serial.print("GREEN: ");
+  Serial.println(params.mGreen);
+
+  Serial.print("BLUE: ");
+  Serial.println(params.mBlue);
+
+  // Speed in ws218fx is defined roughly as an int from 0-3000 with 3000 being the SLOWEST.
+  // So we normalize our 1-100 to that by calculating what percentage of 3000 we are, but
+  // you know..reversed
+  float normalizedSpeed = (float)params.mSpeed/100;
+  normalizedSpeed = 1 - normalizedSpeed;
+  normalizedSpeed *= 3000;
+
+  Serial.print("NORMALIZED SPEED: ");
+  Serial.println(normalizedSpeed);
+  
   ws2812fx.stop();
   ws2812fx.setMode(mode);
+  ws2812fx.setSpeed(normalizedSpeed);
+  ws2812fx.setBrightness(params.mBrightness);
+  ws2812fx.setColor(params.mRed, params.mGreen, params.mBlue);
   ws2812fx.start();
 
   reset();
 }
 
-void readAndDispatchFromBluetooth() {
+void readAndDispatchFromBluetooth()
+{
   if ( Bluefruit.connected() && bleuart.notifyEnabled() )
   {
     while ( bleuart.available() )
@@ -158,17 +280,10 @@ void readAndDispatchFromBluetooth() {
       //The idea here is to read one byte at a time until we hit  a 32 bit null
       //terminator. Then we can dispatch the command to do stuff.
       bleuart.read(dataBuffer + dataOffset, 1);
-      Serial.print("DATA: ");
-      Serial.println((const char*)dataBuffer);
-
       const char lastChar = (const char)(*(dataBuffer + dataOffset));
-      Serial.print("LAST CHAR: ");
-      Serial.println(lastChar);
-
+    
       dataOffset++;
-      Serial.print("DATA OFFSET: ");
-      Serial.println(dataOffset);
-
+      
       if (lastChar == 0) {
         Serial.println("FOUND TERMINATOR");
         runCommand();
@@ -177,7 +292,8 @@ void readAndDispatchFromBluetooth() {
   }
 }
 
-void loop() {
+void loop()
+{
   readAndDispatchFromBluetooth();
   ws2812fx.service();
   waitForEvent();
