21d92c91170dd93d0efaa78fcd08d179a6833c23
Nicholas Pleis <npleis@riotgames.com>
2018-08-14 18:06:04 -0700

    Heavily optimized things
    - Moved the mapping of four character modes to mode numbers into
    the iOS app. This lets us ditch the large map on the feather side
    
    - Moved to a binary protocol instead of sending string. This lets
    us decrease the MTU on the feather side. It also allows us to
    greatly decrease the buffer size as well as eliminate the parsing
    logic altogether. This is a big overall savings in terms of
    memory usage
    
    - Moved the bluetooth library to low  bandwidth mode. Thanks to the
    savings above we can use a bit less memory here.

--- HiveControl/Basic Chat/UartModuleViewController.swift
+++ HiveControl/Basic Chat/UartModuleViewController.swift
@@ -6,12 +6,70 @@
 //  Copyright © 2016 Vanguard Logic LLC. All rights reserved.
 //
 
-
-
-
-
 import UIKit
 import CoreBluetooth
+
+var modeMap = [
+    "MOST" : 0,   // FX_MODE_STATIC
+    "BLCL" : 1,   // FX_MODE_BLINK
+    "MOBR" : 2,   // FX_MODE_BREATH
+    "CLWI" : 3,   // FX_MODE_CLWI
+    "MOIW" : 4,   // FX_MODE_COLOR_WIPE_INV
+    "MORW" : 5,   // FX_MODE_COLOR_WIPE_REV
+    "IRWP" : 6,   // FX_MODE_COLOR_WIPE_REV_INV
+    "RAWP" : 7,   // FX_MODE_COLOR_WIPE_RANDOM
+    "RACL" : 8,   // FX_MODE_RANDOM_COLOR
+    "MOSD" : 9,   // FX_MODE_SINGLE_DYNAMIC
+    "MOMD" : 10,  // FX_MODE_MULTI_DYNAMIC
+    "MORB" : 11,  // FX_MODE_RAINBOW
+    "RBCY" : 12,  // FX_MODE_RAINBOW_CYCLE
+    "MOMS" : 13,  // FX_MODE_SCAN
+    "MODS" : 14,  // FX_MODE_DUAL_SCAN
+    "MOFD" : 15,  // FX_MODE_FADE
+    "MOTC" : 16,  // FX_MODE_THEATER_CHASE
+    "TCRB" : 17,  // FX_MODE_THEATER_CHASE_RAINBOW
+    "MORL" : 18,  // FX_MODE_RUNNING_LIGHTS
+    "MOTK" : 19,  // FX_MODE_TWINKLE
+    "TKRD" : 20,  // FX_MODE_TWINKLE_RANDOM
+    "TKFD" : 21,  // FX_MODE_TWINKLE_FADE
+    "TFDR" : 22,  // FX_MODE_TWINKLE_FADE_RANDOM
+    "MOSP" : 23,  // FX_MODE_SPARKLE
+    "FLSP" : 24,  // FX_MODE_FLASH_SPARKLE
+    "HYSP" : 25,  // FX_MODE_HYPER_SPARKLE
+    "MSTR" : 26,  // FX_MODE_STROBE
+    "STRB" : 27,  // FX_MODE_STROBE_RAINBOW
+    "STMU" : 28,  // FX_MODE_MULTI_STROBE
+    "BLRB" : 29,  // FX_MODE_BLINK_RAINBOW
+    "CHWT" : 30,  // FX_MODE_CHASE_WHITE
+    "CHCL" : 31,  // FX_MODE_CHASE_COLOR
+    "CHRD" : 32,  // FX_MODE_CHASE_RANDOM
+    "CHRB" : 33,  // FX_MODE_CHASE_RAINBOW
+    "CHFL" : 34,  // FX_MODE_CHASE_FLASH
+    "CHFR" : 35,  // FX_MODE_CHASE_FLASH_RANDOM
+    "CHRW" : 36,  // FX_MODE_CHASE_RAINBOW_WHITE
+    "CHBL" : 37,  // FX_MODE_CHASE_BLACKOUT
+    "CHBR" : 38,  // FX_MODE_CHASE_BLACKOUT_RAINBOW
+    "CLSR" : 39,  // FX_MODE_COLOR_SWEEP_RANDOM
+    "MORC" : 40,  // FX_MODE_RUNNING_COLOR
+    "RNRB" : 41,  // FX_MODE_RUNNING_RED_BLUE
+    "RNRD" : 42,  // FX_MODE_RUNNING_RANDOM
+    "LRSC" : 43,  // FX_MODE_LARSON_SCANNER
+    "MOCT" : 44,  // FX_MODE_COMET
+    "MOFW" : 45,  // FX_MODE_FIREWORKS
+    "FWRD" : 46,  // FX_MODE_FIREWORKS_RANDOM
+    "CHRS" : 47,  // FX_MODE_MERRY_CHRISTMAS
+    "MOFF" : 48,  // FX_MODE_FIRE_FLICKER
+    "FFST" : 49,  // FX_MODE_FIRE_FLICKER_SOFT
+    "FFIN" : 50,  // FX_MODE_FIRE_FLICKER_INTENSE
+    "MOCC" : 51,  // FX_MODE_CIRCUS_COMBUSTUS
+    "MOHW" : 52,  // FX_MODE_HALLOWEEN
+    "BICH" : 53,  // FX_MODE_BICOLOR_CHASE
+    "TRCH" : 54,  // FX_MODE_TRICOLOR_CHASE
+    "MOIC" : 55,  // FX_MODE_ICU
+    "OFFF" : 255, // CUSTOM_MODE_OFF
+    "MAXX" : 254, // CUSTOM_MODE_MAX
+    "BEEE" : 253  // CUSTOM_MODE_BEE
+]
 
 class UartModuleViewController: UIViewController, CBPeripheralManagerDelegate, UITextViewDelegate, UITextFieldDelegate {
     
@@ -91,46 +149,46 @@ class UartModuleViewController: UIViewController, CBPeripheralManagerDelegate, U
     }
     
     @IBAction func clickSendAction(_ sender: AnyObject) {
-        outgoingData(inputText: inputTextField.text!)
+        outgoingData(mode: inputTextField.text!)
         
     }
     
     @IBAction func button1(_ sender: UIButton) {
-        
-        outgoingData (inputText: "TCRB" + "," + String(speed) + "," + String(bright) + "," + String(red) + "," + String(green) + "," + String(blue))
+        outgoingData (mode: "TCRB", speed : speed, brightness : bright, red : red, green : green, blue : blue);
     }
     
     @IBAction func button2(_ sender: UIButton) {
-        outgoingData (inputText: "TFDR" + "," + String(speed) + "," + String(bright) + "," + String(red) + "," + String(green) + "," + String(blue))
+        outgoingData (mode: "TFDR", speed : speed, brightness : bright, red : red, green : green, blue : blue);
         
     }
     @IBAction func button3(_ sender: UIButton) {
-         outgoingData (inputText: "RBCY" + "," + String(speed) + "," + String(bright) + "," + String(red) + "," + String(green) + "," + String(blue))
+         outgoingData (mode: "RBCY", speed : speed, brightness : bright, red : red, green : green, blue : blue);
     }
     @IBAction func button4(_ sender: Any) {
-        outgoingData (inputText: "MOHW" + "," + String(speed) + "," + String(bright) + "," + String(red) + "," + String(green) + "," + String(blue))
+        outgoingData (mode: "MOHW", speed : speed, brightness : bright, red : red, green : green, blue : blue);
     }
     @IBAction func button5(_ sender: UIButton) {
-        outgoingData (inputText: "MOCC" + "," + String(speed) + "," + String(bright) + "," + String(red) + "," + String(green) + "," + String(blue))
+        outgoingData (mode: "MOCC", speed : speed, brightness : bright, red : red, green : green, blue : blue);
     }
     
     @IBAction func button6(_ sender: UIButton) {
-        outgoingData (inputText: "LRSC" + "," + String(speed) + "," + String(bright) + "," + String(red) + "," + String(green) + "," + String(blue))
+        outgoingData (mode: "LRSC", speed : speed, brightness : bright, red : red, green : green, blue : blue);
     }
     
     @IBAction func button7(_ sender: UIButton) {
-        outgoingData (inputText: "TKRD" + "," + String(speed) + "," + String(bright) + "," + String(red) + "," + String(green) + "," + String(blue))
+        outgoingData (mode: "TKRD", speed : speed, brightness : bright, red : red, green : green, blue : blue);
     }
 
     @IBAction func button8(_ sender: UIButton) {
+        outgoingData(mode: "BEEE", speed : speed, brightness : bright, red : red, green : green, blue : blue);
     }
     
     @IBAction func maxButton(_ sender: UIButton) {
-        outgoingData(inputText: "MAXX" + "," + String(speed) + "," + String(bright) + "," + String(red) + "," + String(green) + "," + String(blue));
+        outgoingData(mode: "MAXX", speed : speed, brightness : bright, red : red, green : green, blue : blue);
     }
     
     @IBAction func offButton(_ sender: UIButton) {
-        outgoingData(inputText: "OFFF" + "," + String(speed) + "," + String(bright) + "," + String(red) + "," + String(green) + "," + String(blue));
+        outgoingData(mode: "OFFF", speed : speed, brightness : bright, red : red, green : green, blue : blue);
     }
     
     @IBAction func speedSlider(_ sender: UISlider) {
@@ -138,7 +196,6 @@ class UartModuleViewController: UIViewController, CBPeripheralManagerDelegate, U
     }
     
     @IBAction func colorSlider(_ sender: GradientSlider) {
-        
         selectedColor = UIColor(hue: sender.value, saturation: 1.0, brightness: 1.0, alpha: 1.0)
         sender.thumbColor = selectedColor
        
@@ -154,92 +211,88 @@ class UartModuleViewController: UIViewController, CBPeripheralManagerDelegate, U
 
     @IBAction func brightSlider(_ sender: UISlider) {
         bright = Int(sender.value)
-        
     }
     
-    func outgoingData (inputText : String) {
+    func outgoingData (mode : String, speed : Int = 50, brightness: Int = 50, red: Int = 255, green: Int = 0, blue : Int = 0) {
         let appendString = "\n"
-        
-       
-       //let inputText = inputTextField.text
-        //var dataString = inputText + "\0"
-        //var timeStampString = "\(CACurrentMediaTime())" + "\0"
-       //print (timeStampString)
-        
-        
-        
+
         var totalDevices = peripheralsArray.count
         var counter = 0
         var mapIndex = 0
-        var executeInMs = totalDevices * 30; //we budget 30ms transmission time per device
+        var executeInMs:UInt16 = UInt16(totalDevices) * 30; //we budget 30ms transmission time per device
+        var displayString = ""
 
-        while counter < totalDevices
-        {
-            var sendText = inputText;
-            sendText += "," + String(executeInMs);
-            executeInMs -= 30;
+        if let modeId = modeMap[mode] {
+            displayString = "Mode: " + mode;
+            displayString += ", Speed: " + String(speed);
+            displayString += ", Bright: " + String(bright);
+            displayString += ", Red: " + String(red);
+            displayString += ", Blue: " + String(blue);
+            displayString += ", Green: " + String(green);
 
-            print ("Sending Data to peripheral:")
-            blePeripheral = peripheralsArray[counter]
-            print (counter)
-            var mystring = blePeripheral?.identifier.uuidString
-             mapIndex = mapArray.index(of: mystring)!
-            print (mystring)
-            print (mapIndex)
-            txCharacteristic = txArray[mapIndex]
-            print (blePeripheral)
-            
-            writeValue(data: sendText + "\0")
-            print("                    ")
-            print (txCharacteristic)
-            print("                   ")
-            //writeValue(data: timeStampString)
-            counter = counter + 1
+            while counter < totalDevices
+            {
+                // We send an 8 byte binary data stream that looks like:
+                // 1 byte: mode
+                // 1 byte: speed
+                // 1 byte: brightness
+                // 1 byte: red
+                // 1 byte: green
+                // 1 byte: blue
+                // 2 bytes: milliseconds into the future to execute at
+                
+                var writeData:Data = Data();
+                writeData.append(UInt8(modeId));
+                writeData.append(UInt8(speed));
+                writeData.append(UInt8(brightness));
+                writeData.append(UInt8(red));
+                writeData.append(UInt8(green));
+                writeData.append(UInt8(blue));
+                writeData.append(UnsafeBufferPointer(start: &executeInMs, count: 1));
+                
+                print ("Sending Data to peripheral:")
+                blePeripheral = peripheralsArray[counter]
+                print (counter)
+                var mystring = blePeripheral?.identifier.uuidString
+                mapIndex = mapArray.index(of: mystring)!
+                print (mystring)
+                print (mapIndex)
+                txCharacteristic = txArray[mapIndex]
+                print (blePeripheral)
+                
+                writeValue(data: writeData);
+                print("                    ")
+                print (txCharacteristic)
+                print("                   ")
+                //writeValue(data: timeStampString)
+                counter = counter + 1
+            }
+        } else {
+            displayString = "ERROR: Unknown mode " + mode;
         }
         
         let myFont = UIFont(name: "Helvetica Neue", size: 15.0)
         let myAttributes1 = [NSAttributedStringKey.font: myFont!, NSAttributedStringKey.foregroundColor: UIColor.blue]
-       
-        
-        
-      
-       
-        
-        let attribString = NSAttributedString(string: "[Outgoing]: " + inputText + appendString, attributes: myAttributes1)
+
+        let attribString = NSAttributedString(string: "[Outgoing]: " + displayString + appendString, attributes: myAttributes1)
         let newAsciiText = NSMutableAttributedString(attributedString: self.consoleAsciiText!)
         newAsciiText.append(attribString)
-        
-        
-        
+
         consoleAsciiText = attribString
-        debugLabel.text = inputText
-        //baseTextView.attributedText = consoleAsciiText
-        //erase what's in the text field
-        //inputTextField.text = ""
-        
+        debugLabel.text = displayString
     }
     
     // Write functions
-    func writeValue(data: String){
-        let valueString = (data as NSString).data(using: String.Encoding.utf8.rawValue)
-        
-     
-     print ("TX Char ________________________________________________________")
-        
+    func writeValue(data: Data){
+        print ("TX Char ________________________________________________________")
         print (txCharacteristic)
+        print ("TX Char ________________________________________________________")
         
-       print ("TX Char ________________________________________________________")
-             //change the "data" to valueString
-        
-            if let blePeripheral = blePeripheral{
-                if let txCharacteristic = txCharacteristic {
-                    blePeripheral.writeValue(valueString!, for: txCharacteristic, type: CBCharacteristicWriteType.withResponse)
-                }
+        if let blePeripheral = blePeripheral{
+            if let txCharacteristic = txCharacteristic {
+                blePeripheral.writeValue(data, for: txCharacteristic, type: CBCharacteristicWriteType.withResponse)
             }
-        
-        
-       
-        
+        }
     }
     
     func writeCharacteristic(val: Int8){
@@ -247,9 +300,7 @@ class UartModuleViewController: UIViewController, CBPeripheralManagerDelegate, U
         let ns = NSData(bytes: &val, length: MemoryLayout<Int8>.size)
         blePeripheral!.writeValue(ns as Data, for: txCharacteristic!, type: CBCharacteristicWriteType.withResponse)
     }
-    
-    
-    
+
     //MARK: UITextViewDelegate methods
     func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
         /*if textView === baseTextView {
